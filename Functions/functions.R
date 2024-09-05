855bb20391a37cf3a5d3a2a865a32abe2e25ef7e
Shai <105252809+OlivazShai@users.noreply.github.com>
2024-09-05 10:47:23 -0300

    create trade data

--- Functions/functions.R
+++ Functions/functions.R
@@ -1,6 +1,6 @@
 # Function to write multiple objects to .rds files
 
-  bulk_rds <- function(...) {
+  bulk_write_rds <- function(...) {
     # Load library
     library(readr)
     # Capture the names of the objects passed to the function
@@ -16,4 +16,27 @@
     }
   }
 
-# Next functions...
+# Function to read .rds files from Variables
+  
+  bulk_read_rds <- function(...) {
+    # Collect object names from the function arguments
+    object_names <- as.character(match.call(expand.dots = FALSE)$`...`)
+    
+    # Loop through each object name
+    for (obj_name in object_names) {
+      # Construct the file path
+      file_path <- file.path("../Variables", paste0(obj_name, ".rds"))
+      
+      # Check if the file exists
+      if (!file.exists(file_path)) {
+        stop(paste("File does not exist:", file_path))
+      }
+      
+      # Load the RDS file
+      loaded_data <- readRDS(file_path)
+      
+      # Assign the loaded data to an object with the same name
+      assign(obj_name, loaded_data, envir = .GlobalEnv)
+    }
+  }
+  
