7d54448ba8689fba97d59364e3bc76a1054ad86c
Shai <105252809+OlivazShai@users.noreply.github.com>
2025-01-19 22:51:28 -0300

    Update tax_counterfactual.R

--- Functions/tax_counterfactual.R
+++ Functions/tax_counterfactual.R
@@ -1,4 +1,4 @@
-tax_counterfactual <- function(
+y_tax <- function(
     int_mod, 
     ext_mod,
     ext_cf,
@@ -24,8 +24,62 @@ tax_counterfactual <- function(
   
   # Add the fixed effects back to the predictions
   fixed_effects <- predict(ext_mod, newdata = ext_cf, fixef = TRUE)$main_biome
-  y_biomass <- y_biomass + fixed_effects
+  y_tax <- y_biomass + fixed_effects
   
   # Return the predictions
-  return(y_biomass)
+  return(y_tax)
+}
+
+
+tax_counterfactual <- function(
+    int_mod, 
+    ext_mod,
+    int_data,
+    ext_cf,
+    tax
+    ) {
+  
+  # Get baseline deforestation estimate
+  hats = recovery_counterfactual(
+    int_mod = int_mod, 
+    ext_mod = ext_mod, 
+    int_data = int_data, 
+    ext_cf = ext_cf, 
+    min_high_share = 0,
+  ) |>
+    filter(type == "static") |>
+    select(hat, hat_co2)
+  
+  ############# Calculate probabilities and aggregate ############
+  
+  result <- ext_cf |>
+    mutate(
+      y_tax = y_tax(
+        int_mod = int_mod, 
+        ext_mod = ext_mod,
+        ext_cf = ext_cf, 
+        tax = tax),
+    ) |> 
+    select(
+      code_amc, year, rho_1, rho_2, y_tax, natural, biomass_density
+    ) |> 
+    mutate(
+      across(
+        .cols = c(y_tax),
+        .fns = ~ logit(.x - 0.9*gamma + 0.9*log(rho_2)),
+        .names = "rho_{.col}")
+    ) |> 
+    filter(
+      year %in% c(2006, 2017)
+    ) |> 
+    summarise(
+      # baseline
+      hat = hats$hat,
+      hat_co2 = hats$hat_co2,
+      # counterfactual
+      tax = sum(rho_y_tax*natural),
+      tax_co2 = sum(rho_y_tax*natural*biomass_density) / 10**9
+    )
+  
+  return(result)
 }
